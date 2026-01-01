6f586aa5b23d0cbf30fd35068dca587a5b3a7897
Shuhei Akutagawa <shuhei.akutagawa+github@gmail.com>
2026-01-01 09:47:18 +0900

    Add English sentence definition (#7)

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -1,16 +1,24 @@
 import assert from "node:assert/strict";
 import test from "node:test";
 
-import { Character, Sentence, Session, createJapaneseSentenceDefinition } from "./index.ts";
+import {
+  Character,
+  Sentence,
+  Session,
+  createEnglishSentenceDefinition,
+  createJapaneseSentenceDefinition,
+} from "./index.ts";
 import { Character as DirectCharacter } from "./character.ts";
 import { Sentence as DirectSentence } from "./sentence.ts";
 import { Session as DirectSession } from "./session.ts";
+import { createEnglishSentenceDefinition as DirectCreateEnglishSentenceDefinition } from "./createEnglishSentenceDefinition.ts";
 import { createJapaneseSentenceDefinition as DirectCreateJapaneseSentenceDefinition } from "./createJapaneseSentenceDefinition.ts";
 
 test("index re-exports runtime modules", () => {
   assert.equal(Character, DirectCharacter);
   assert.equal(Sentence, DirectSentence);
   assert.equal(Session, DirectSession);
+  assert.equal(createEnglishSentenceDefinition, DirectCreateEnglishSentenceDefinition);
   assert.equal(createJapaneseSentenceDefinition, DirectCreateJapaneseSentenceDefinition);
 });
 
